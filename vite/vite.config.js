0561a7c5b1ec6028780e4f1cbb80615ff3cab872
Mitsuyama <mitsuyama@163.com>
2023-03-13 22:06:26 +0800

    feat: build to /docs directory

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
-const path = require("path");
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "/src") }],
+    alias: [{ find: '@', replacement: path.resolve(__dirname, '/src') }],
   },
   plugins: [reactRefresh()],
+  build: {
+    outDir: path.resolve(__dirname, '../docs/'),
+  },
 });
